bc6e5241196a47d0f7311496fa1649381b3f13a6
ushwlwvw <126856143+ushwlwvw@users.noreply.github.com>
2023-03-25 07:10:04 +0300

    Update stop.py

--- FallenMusic/Modules/stop.py
+++ FallenMusic/Modules/stop.py
@@ -27,7 +27,7 @@ from FallenMusic import app, pytgcalls
 from FallenMusic.Helpers import _clear_, admin_check, close_key
 
 
-@app.on_message(filters.command(["stop", "end"]) | filters.command(["ايقاف","قف"],prefixes= ["/", "!","","#"]) & filters.group)
+@app.on_message(filters.command(["stop", "end"]) | filters.command(["ايقاف","اوكف"],prefixes= ["/", "!","","#"]) & filters.group)
 @admin_check
 async def stop_str(_, message: Message):
     try:
